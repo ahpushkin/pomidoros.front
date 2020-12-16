5fae040db2d2271fbf3b1b739fe769a68b34f15b
Tymur <realtimur@gmail.com>
2020-12-16 13:44:52 +0200

    Added MapBoxProvider class

---
+++ Pomidoros/Services/MapBoxProvider.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
+using Naxam.Mapbox;
+using Naxam.Mapbox.Annotations;
+using Naxam.Mapbox.Layers;
+using Naxam.Mapbox.Sources;
+using Xamarin.Forms;
+
+namespace Pomidoros.Services
+{
+    public class MapBoxProvider
+    {
+        public static LatLng InitialCenter { get; } = new LatLng(49.9977729, 36.2413953);
+
+        public double ZoomLevel { get; } = 15;
+
+        public IMapFunctions MapFunctions { get; set; }
+
+        public ObservableCollection<Annotation> Annotations { get; } = new ObservableCollection<Annotation>();
+
+        public void AddEndPoints(IList<IPosition> routeCoordinates)
+        {
+            AddEndPoints(routeCoordinates, Annotations, "geo.png", "geo2.png");
+        }
+
+        public void AddRoute(IEnumerable<IPosition> routeCoordinates)
+        {
+            AddRoute(routeCoordinates, MapFunctions, (Color)Application.Current.Resources["mainColor"]);
+        }
+
+        void AddEndPoints(IList<IPosition> routeCoordinates, IList<Annotation> annotations, ImageSource startImage, ImageSource endImage)
+        {
+            var start = routeCoordinates[0];
+            var end = routeCoordinates[routeCoordinates.Count - 1];
+
+            var startAnnotation = new SymbolAnnotation()
+            {
+                Coordinates = new LatLng(start.Latitude, start.Longitude),
+                IconImage = startImage,
+                IconSize = 1
+            };
+            annotations.Add(startAnnotation);
+
+            var endAnnotation = new SymbolAnnotation()
+            {
+                Coordinates = new LatLng(end.Latitude, end.Longitude),
+                IconImage = endImage,
+                IconSize = 2
+            };
+            annotations.Add(endAnnotation);
+        }
+
+        void AddRoute(IEnumerable<IPosition> routeCoordinates, IMapFunctions mapFunctions, Color lineColor)
+        {
+            var featureCollection = new FeatureCollection();
+            var geometry = new LineString(routeCoordinates);
+            var feature = new Feature(geometry);
+            featureCollection.Features.Add(feature);
+
+            var sourceId = Guid.NewGuid().ToString();
+            var source = new GeoJsonSource()
+            {
+                Id = sourceId,
+                Data = featureCollection
+            };
+            mapFunctions.AddSource(source);
+
+            var lineLayer = new LineLayer(Guid.NewGuid().ToString(), sourceId)
+            {
+                LineWidth = 5,
+                LineColor = lineColor
+            };
+            mapFunctions.AddLayer(lineLayer);
+        }
+    }
+}
--- Pomidoros/ViewModel/Orders/ReviewOrderPageViewModel.cs
+++ Pomidoros/ViewModel/Orders/ReviewOrderPageViewModel.cs
@@ -1,20 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Core.Commands;
 using Core.Extensions;
 using Core.Navigation;
-using GeoJSON.Net.Feature;
 using GeoJSON.Net.Geometry;
 using Naxam.Controls.Forms;
 using Naxam.Mapbox;
-using Naxam.Mapbox.Annotations;
-using Naxam.Mapbox.Layers;
-using Naxam.Mapbox.Sources;
 using Pomidoros.Resources;
+using Pomidoros.Services;
 using Pomidoros.View;
 using Pomidoros.View.Orders;
 using Pomidoros.ViewModel.Base;
@@ -27,8 +21,6 @@ namespace Pomidoros.ViewModel.Orders
     {
         public ReviewOrderPageViewModel()
         {
-            ZoomLevel = 15;
-
             DidFinishLoadingStyleCommand = new Command<MapStyle>(DidFinishLoadingStyle);
         }
 
@@ -48,13 +40,9 @@ namespace Pomidoros.ViewModel.Orders
             set => SetProperty(ref _order, value);
         }
 
-        public IMapFunctions MapFunctions { get; set; }
+        public MapBoxProvider MapBoxProvider { get; } = new MapBoxProvider();
 
-        public ObservableCollection<Annotation> Annotations { get; } = new ObservableCollection<Annotation>();
-
-        public double ZoomLevel { get; }
-
-        LatLng _center = new LatLng(49.9977729, 36.2413953);
+        LatLng _center = MapBoxProvider.InitialCenter;
         public LatLng Center
         {
             get => _center;
@@ -73,60 +61,15 @@ namespace Pomidoros.ViewModel.Orders
             {
                 Center = new LatLng(routeCoordinates[0].Latitude, routeCoordinates[0].Longitude);
 
-                AddEndPoints(routeCoordinates, Annotations, "geo.png", "geo2.png");
+                MapBoxProvider.AddEndPoints(routeCoordinates);
 
-                AddRoute(routeCoordinates, MapFunctions, Color.FromHex("#96A637"));
+                MapBoxProvider.AddRoute(routeCoordinates);
             }
-        }
-
-        void AddEndPoints(IList<IPosition> routeCoordinates, IList<Annotation> annotations, ImageSource startImage, ImageSource endImage)
-        {
-            var start = routeCoordinates[0];
-            var end = routeCoordinates[routeCoordinates.Count - 1];
-
-            var startAnnotation = new SymbolAnnotation()
-            {
-                Coordinates = new LatLng(start.Latitude, start.Longitude),
-                IconImage = startImage,
-                IconSize = 1
-            };
-            annotations.Add(startAnnotation);
-
-            var endAnnotation = new SymbolAnnotation()
-            {
-                Coordinates = new LatLng(end.Latitude, end.Longitude),
-                IconImage = endImage,
-                IconSize = 2
-            };
-            annotations.Add(endAnnotation);
-        }
-
-        void AddRoute(IEnumerable<IPosition> routeCoordinates, IMapFunctions mapFunctions, Color lineColor)
-        {
-            var featureCollection = new FeatureCollection();
-            var geometry = new LineString(routeCoordinates);
-            var feature = new Feature(geometry);
-            featureCollection.Features.Add(feature);
-
-            var sourceId = Guid.NewGuid().ToString();
-            var source = new GeoJsonSource()
-            {
-                Id = sourceId,
-                Data = featureCollection
-            };
-            mapFunctions.AddSource(source);
-
-            var lineLayer = new LineLayer(Guid.NewGuid().ToString(), sourceId)
-            {
-                LineWidth = 5,
-                LineColor = lineColor
-            };
-            mapFunctions.AddLayer(lineLayer);
         }
 
         private Task OnShowRouteCommand(object arg)
         {
-            return Navigation.PushAsync(new MapPage());
+            return Navigation.PushAsync(new MapPage(), Order, "order");
         }
 
         private Task OnOrderContentCommand(object arg)
